810bbb67431e750d5505ff629b84877e77bd2545
Achmad Ardiansyah <theadods@gmail.com>
2023-01-04 16:39:54 +0700

    - Add field check on "down" method in "move_avatar" migration - Add reverse renaming on "down" method in "change_all_table_prefix" migration

--- src/database/migrations/2019_07_11_231939_move_avatar_column_to_users_table.php
+++ src/database/migrations/2019_07_11_231939_move_avatar_column_to_users_table.php
@@ -8,7 +8,7 @@ use App\Base\Traits\MigrateDataTenant;
 class MoveAvatarColumnToUsersTable extends Migration
 {
     use MigrateDataTenant;
-    
+
     /**
      * Run the migrations.
      *
@@ -16,27 +16,27 @@ class MoveAvatarColumnToUsersTable extends Migration
      */
     public function up()
     {
-        if(config('AppConfig.system.multitenant.active',false) && $this->tenantMigrateMode()==false){
-            if (!Schema::hasColumn('users','avatar')) {
+        if (config('AppConfig.system.multitenant.active', false) && $this->tenantMigrateMode() == false) {
+            if (!Schema::hasColumn('users', 'avatar')) {
                 Schema::table('users', function (Blueprint $table) {
                     $table->string('avatar')->after('user_idcode')->default('');
                 });
             }
-            
-            if (Schema::hasColumn('user_profiles','avatar')) {
+
+            if (Schema::hasColumn('user_profiles', 'avatar')) {
                 Schema::table('user_profiles', function (Blueprint $table) {
                     $table->dropColumn('avatar');
-                });     
-            }   
+                });
+            }
         }
 
         $this->tablePerTenant('users', function (Blueprint $table) {
             $table->string('avatar')->after('user_idcode')->default('');
-        },'avatar');
+        }, 'avatar');
 
         $this->tablePerTenant('user_profiles', function (Blueprint $table) {
             $table->dropColumn('avatar');
-        },'avatar',true);
+        }, 'avatar', true);
     }
 
     /**
@@ -46,11 +46,15 @@ class MoveAvatarColumnToUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('avatar');
-        });
-        Schema::table('user_profiles', function (Blueprint $table) {
-            $table->string('avatar')->after('user_id')->default('');
-        });
+        if (Schema::hasColumn('users', 'avatar')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('avatar');
+            });
+        }
+        if (!Schema::hasColumn('user_profiles', 'avatar')) {
+            Schema::table('user_profiles', function (Blueprint $table) {
+                $table->string('avatar')->after('user_id')->default('');
+            });
+        }
     }
 }
--- src/database/migrations/2021_12_03_045229_change_all_table_prefix_to_moduser.php
+++ src/database/migrations/2021_12_03_045229_change_all_table_prefix_to_moduser.php
@@ -8,7 +8,7 @@ use App\Base\Traits\MigrateDataTenant;
 class ChangeAllTablePrefixToModuser extends Migration
 {
     use MigrateDataTenant;
-    
+
     /**
      * Run the migrations.
      *
@@ -16,35 +16,35 @@ class ChangeAllTablePrefixToModuser extends Migration
      */
     public function up()
     {
-        if(config('AppConfig.system.multitenant.active',false) && $this->tenantMigrateMode()==false){
-            if(Schema::hasTable('api_tokens')){
-                Schema::rename('api_tokens','moduser_api_tokens');
-                Schema::rename('auth_logs','moduser_auth_logs');
-                Schema::rename('notifications','moduser_notifications');
-                Schema::rename('notification_channels','moduser_notification_channels');
-                Schema::rename('password_resets','moduser_password_resets');
-                Schema::rename('roles','moduser_roles');
-                Schema::rename('system_user_logs','moduser_system_user_logs');
-                Schema::rename('users','moduser_users');
-                Schema::rename('user_otp','moduser_user_otp');
-                Schema::rename('user_profiles','moduser_user_profiles');
-                Schema::rename('user_roles','moduser_user_roles');
-                Schema::rename('user_tenants','moduser_user_tenants');
+        if (config('AppConfig.system.multitenant.active', false) && $this->tenantMigrateMode() == false) {
+            if (Schema::hasTable('api_tokens')) {
+                Schema::rename('api_tokens', 'moduser_api_tokens');
+                Schema::rename('auth_logs', 'moduser_auth_logs');
+                Schema::rename('notifications', 'moduser_notifications');
+                Schema::rename('notification_channels', 'moduser_notification_channels');
+                Schema::rename('password_resets', 'moduser_password_resets');
+                Schema::rename('roles', 'moduser_roles');
+                Schema::rename('system_user_logs', 'moduser_system_user_logs');
+                Schema::rename('users', 'moduser_users');
+                Schema::rename('user_otp', 'moduser_user_otp');
+                Schema::rename('user_profiles', 'moduser_user_profiles');
+                Schema::rename('user_roles', 'moduser_user_roles');
+                Schema::rename('user_tenants', 'moduser_user_tenants');
             }
         }
-        
-        $this->renameTablePerTenant('api_tokens','moduser_api_tokens');
-        $this->renameTablePerTenant('auth_logs','moduser_auth_logs');
-        $this->renameTablePerTenant('notifications','moduser_notifications');
-        $this->renameTablePerTenant('notification_channels','moduser_notification_channels');
-        $this->renameTablePerTenant('password_resets','moduser_password_resets');
-        $this->renameTablePerTenant('roles','moduser_roles');
-        $this->renameTablePerTenant('system_user_logs','moduser_system_user_logs');
-        $this->renameTablePerTenant('users','moduser_users');
-        $this->renameTablePerTenant('user_otp','moduser_user_otp');
-        $this->renameTablePerTenant('user_profiles','moduser_user_profiles');
-        $this->renameTablePerTenant('user_roles','moduser_user_roles');
-        $this->renameTablePerTenant('user_tenants','moduser_user_tenants');
+
+        $this->renameTablePerTenant('api_tokens', 'moduser_api_tokens');
+        $this->renameTablePerTenant('auth_logs', 'moduser_auth_logs');
+        $this->renameTablePerTenant('notifications', 'moduser_notifications');
+        $this->renameTablePerTenant('notification_channels', 'moduser_notification_channels');
+        $this->renameTablePerTenant('password_resets', 'moduser_password_resets');
+        $this->renameTablePerTenant('roles', 'moduser_roles');
+        $this->renameTablePerTenant('system_user_logs', 'moduser_system_user_logs');
+        $this->renameTablePerTenant('users', 'moduser_users');
+        $this->renameTablePerTenant('user_otp', 'moduser_user_otp');
+        $this->renameTablePerTenant('user_profiles', 'moduser_user_profiles');
+        $this->renameTablePerTenant('user_roles', 'moduser_user_roles');
+        $this->renameTablePerTenant('user_tenants', 'moduser_user_tenants');
     }
 
     /**
@@ -54,8 +54,17 @@ class ChangeAllTablePrefixToModuser extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
+        Schema::rename('moduser_api_tokens', 'api_tokens');
+        Schema::rename('moduser_auth_logs', 'auth_logs');
+        Schema::rename('moduser_notifications', 'notifications');
+        Schema::rename('moduser_notification_channels', 'notification_channels');
+        Schema::rename('moduser_password_resets', 'password_resets');
+        Schema::rename('moduser_roles', 'roles');
+        Schema::rename('moduser_system_user_logs', 'system_user_logs');
+        Schema::rename('moduser_users', 'users');
+        Schema::rename('moduser_user_otp', 'user_otp');
+        Schema::rename('moduser_user_profiles', 'user_profiles');
+        Schema::rename('moduser_user_roles', 'user_roles');
+        Schema::rename('moduser_user_tenants', 'user_tenants');
     }
 }
