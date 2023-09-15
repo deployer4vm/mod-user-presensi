8bdc2c61fcc2450e6a9440f5aff12c4851daad31
Harry Prayudhi <kang.harry.p@gmail.com>
2023-09-15 21:03:16 +0700

    add pusher notification

--- src/resources/js/store/store.js
+++ src/resources/js/store/store.js
@@ -3,5 +3,6 @@ import userStore from "./modules/user";
 import roleStore from "./modules/role";
 import usersystem from "./modules/usersystem";
 import rolesystem from "./modules/rolesystem";
+import moduserView from "./storeView";
 
-export default {user: userStore,role: roleStore,auth: authStore,usersystem: usersystem,rolesystem: rolesystem};
+export default {moduserView, user: userStore,role: roleStore,auth: authStore,usersystem: usersystem,rolesystem: rolesystem};
