bc786f6877e31e901968f80a0f288ce4e8ed259e
AbdulMalikMuzakir <abdulmalikmuzakir55@gmail.com>
2023-09-17 22:14:23 +0700

    test pusher

--- src/Controllers/BroadcastController.php
+++ src/Controllers/BroadcastController.php
@@ -13,6 +13,9 @@ use App\Base\BaseController;
 
 use App\Events\SendData;
 
+use Pusher\Pusher;
+use GuzzleHttp\Client;
+
 class BroadcastController extends BaseController
 {
     protected $accessRuleKey = 'moduser.broadcast';
@@ -29,18 +32,39 @@ class BroadcastController extends BaseController
     
     public function sendBroadcast(Request $request)
     {
-        event(new SendData('data'));
+        // Mengatur opsi verify ke false untuk Guzzle client
+        $client = new Client(['verify' => false]);
+
+        // Inisialisasi objek Pusher
+        $pusher = new Pusher(
+            'ddeff2d2899cc7d9d641',
+            'f155bfdc00eaec781ec3',
+            '1669062',
+            [
+                'cluster' => 'ap1',
+                'useTLS' => true
+            ]
+        );
+
+        $input = $request->only(['title', 'description', 'message']);
+
+        // Data yang akan dikirim
+        $data['message'] = $input;
+
+        // Mengirim pesan ke Pusher
+        $pusher->trigger('my-channel', 'my-event', $data);
+
         return response()->json([
             'data' => 'test'
         ]);
 
-        if(!UserAuth::hasAccess($this->accessRuleKey,'c')){
-            $this->setError(__('alert.access_denied',false,403));
+        if (!UserAuth::hasAccess($this->accessRuleKey, 'c')) {
+            $this->setError(__('alert.access_denied', false, 403));
             return $this->done();
         }
 
-        $input = $request->only(['title','description','message']);
-        BroadcastNotif::dispatch(UserAuth::user('id'),false,$input['title'],$input['description'],$input['message']);
+        $input = $request->only(['title', 'description', 'message']);
+        BroadcastNotif::dispatch(UserAuth::user('id'), false, $input['title'], $input['description'], $input['message']);
         $this->output['message'] = 'Broadcast berhasil dikirim';
         return $this->done();
     }
--- src/resources/js/views/admin/user/notiflist.vue
+++ src/resources/js/views/admin/user/notiflist.vue
@@ -1,117 +1,104 @@
 <template>
-    <b-card no-body header="Notification">
-      <b-list-group>
-        <b-list-group-item
-          :to="{ name: item.link_web.route, params: item.link_web.parameter }"
-          class="media d-flex align-items-center"
-          v-for="item in notif.notification"
-          :key="'pagenotif-' + item.id"
-        >
-          <div
-            :class="'ui-icon ui-icon-sm ion ion-ios-text border-0 text-white ' + (item.read_at == null ? 'bg-danger' : 'bg-secondary')"
-          ></div>
-          <div class="media-body line-height-condenced ml-3">
-            <div :class="item.read_at == null ? 'text-dark font-weight-bold' : 'text-muted'">{{ item.data.subject }}</div>
-            <div class="text-light small mt-1">{{ item.data.description }}</div>
-            <div class="text-light small mt-1">{{ item.created_at }}</div>
-          </div>
-        </b-list-group-item>
-      </b-list-group>
-      <div class="float-right m-2 mt-4" v-if="showPagination">
-        <b-pagination
-          size="sm"
-          :total-rows="notif.summary.count"
-          v-model="currentPage"
-          :per-page="perPage"
-          align="center"
-        />
-      </div>
+  <b-card no-body header="Notification">
+    <b-list-group>
+      <b-list-group-item :to="{ name: item.link_web.route, params: item.link_web.parameter }"
+        class="media d-flex align-items-center" v-for="item in notif.notification" :key="'pagenotif-' + item.id">
+        <div
+          :class="'ui-icon ui-icon-sm ion ion-ios-text border-0 text-white ' + (item.read_at == null ? 'bg-danger' : 'bg-secondary')">
+        </div>
+        <div class="media-body line-height-condenced ml-3">
+          <div :class="item.read_at == null ? 'text-dark font-weight-bold' : 'text-muted'">{{ item.data.subject }}</div>
+          <div class="text-light small mt-1">{{ item.data.description }}</div>
+          <div class="text-light small mt-1">{{ item.created_at }}</div>
+        </div>
+      </b-list-group-item>
+    </b-list-group>
+    <div class="float-right m-2 mt-4" v-if="showPagination">
+      <b-pagination size="sm" :total-rows="notif.summary.count" v-model="currentPage" :per-page="perPage"
+        align="center" />
+    </div>
+
+    <div>
+      <ul>
+        <li v-for="(v, k) in messages" :key="k">
+          {{ v }}
+        </li>
+      </ul>
+    </div>
+  </b-card>
+</template>
   
-      <div>
-        <ul>
-          <li v-for="(v, k) in messages" :key="k">
-            {{ v }}
-          </li>
-        </ul>
-      </div>
-    </b-card>
-  </template>
+  <!-- <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script> -->
   
-  <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-  
-  <script>
-  import Pusher from 'pusher-js';
-  import Echo from 'laravel-echo';
-  
-  export default {
-    data() {
-      return {
-        notif: {
-          notification: [],
-          summary: {
-            count: 0,
-            unread_count: 0,
-          },
+<script>
+// import Pusher from 'pusher-js';
+// import Echo from 'laravel-echo';
+
+export default {
+  data() {
+    return {
+      notif: {
+        notification: [],
+        summary: {
+          count: 0,
+          unread_count: 0,
         },
-        currentPage: 1,
-        perPage: 10,
-  
-        messages: [],
-      };
-    },
-    computed: {
-      showPagination() {
-        return this.notif.summary.count > this.perPage;
       },
+      currentPage: 1,
+      perPage: 10,
+
+      messages: [],
+    };
+  },
+  computed: {
+    showPagination() {
+      return this.notif.summary.count > this.perPage;
     },
-    watch: {
-      currentPage(nV, oV) {
-        this.loadNotif((nV - 1) * this.perPage);
-      },
+  },
+  watch: {
+    currentPage(nV, oV) {
+      this.loadNotif((nV - 1) * this.perPage);
     },
-    created() {
-      Pusher.logToConsole = true;
-  
+  },
+  created() {
+      window.Pusher.logToConsole = true;
+
       this.messages = [];
-  
-      this.pusher = new Pusher('ddeff2d2899cc7d9d641', {
-        cluster: 'ap1',
-      });
-  
-      const channel = this.pusher.subscribe('my-channel');
-  
+
+      const channel = window.Pusher.subscribe('my-channel');
+
       channel.bind('my-event', (data) => {
         this.messages.push(JSON.stringify(data));
         this.loadNotif();
       });
 
-      window.Echo = new Echo({
-        broadcaster: 'pusher',
-        key: process.env.MIX_PUSHER_APP_KEY,
-        cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-        encrypted: true,
-    });
+    //   window.Echo = new Echo({
+    //     broadcaster: 'pusher',
+    //     key: process.env.MIX_PUSHER_APP_KEY,
+    //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    //     encrypted: true,
+    // });
 
-      Echo.channel('my-channel')
+    window.Echo.channel('my-channel')
       .listen('my-event', (e) => {
-          console.log(e);
+        console.log(e);
+      });
+  },
+  methods: {
+    loadNotif(offset = 0) {
+      let filterParams = {
+        params: {
+          offset: offset,
+          limit: this.perPage,
+          setRead: true,
+        },
+      };
+      this.LocalApi.get(this.AppConfig.endpoint.api.moduser + '/notification', filterParams).then((res) => {
+        this.notif = res.data.data;
       });
     },
-    methods: {
-      loadNotif(offset = 0) {
-        let filterParams = {
-          params: {
-            offset: offset,
-            limit: this.perPage,
-            setRead: true,
-          },
-        };
-        this.LocalApi.get(this.AppConfig.endpoint.api.moduser + '/notification', filterParams).then((res) => {
-          this.notif = res.data.data;
-        });
-      },
-    },
-  };
-  </script>
+  },
+};
+</script>
   
